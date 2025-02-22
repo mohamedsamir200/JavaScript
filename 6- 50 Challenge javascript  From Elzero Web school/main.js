a29396cdd0194c68275fd3a08e7e9baa44223176
Muhammed Samir <105426114+mohamedsamir200@users.noreply.github.com>
2025-02-23 00:26:21 +0200

    Update main.js

--- 6- 50 Challenge javascript  From Elzero Web school/main.js
+++ 6- 50 Challenge javascript  From Elzero Web school/main.js
@@ -1,40 +1,145 @@
-"=========================  1  ============================"
+// ========> Go To( ReadMi.md )file to show All Assignments <========//
+
+"=========================  1  ============================";
 //#region ======>  1 <===========
+// let fileName = "Elzero.php";
+
+// console.log(fileName.split(".")[0]) // Elzero 
+// console.log(fileName.split(".")[1]) // php
 
 //#endregion
-"=========================  2  ============================"
+"=========================  2  ============================";
 //#region ======>  2 <===========
-
+// function addEl(str) {
+//   return  str === "" || str.startsWith("El") ? str : `El${str}`
+// }
+// console.log(addEl("")); // ""
+// console.log(addEl("Elzero")); // Elzero
+// console.log(addEl("zero")); // Elzero
 //#endregion
-"=========================  3  ============================"
+"=========================  3  ============================";
 //#region ======>  3 <===========
-
+// let myString = "Hello Elzero Web School @ We Love Programming@ @#!@#$%%^&*";
+// console.log(myString.split("@").slice(0, 2).join("@"));
+// // Output Needed
+// "Hello Elzero Web School @ We Love Programming";
 //#endregion
-"=========================  4  ============================"
+"=========================  4  ============================";
 //#region ======>  4 <===========
+// function checkRange(n1, n2, n3, n4, n5) {
+//     return [n1 , n2 , n3 ].every ( (num)=> num >= n4 && num <= n5 )
+//     ? "Yes All Numbers In Range" : "Not All Numbers Is In Range"
+//   }
 
+//   console.log(checkRange(5, 10, 15, 5, 50)); // Yes All Numbers In Range
+//   console.log(checkRange(8, 4, 20, 2, 50)); // Yes All Numbers In Range
+//   console.log(checkRange(10, 15, 20, 5, 18)); // Not All Numbers Is In Range
 //#endregion
-"=========================  5  ============================"
+"=========================  5  ============================";
 //#region ======>  5 <===========
+// function replaceFirstWithLast(word) {
+//     let wordArr = word.split("");
+//     [wordArr[0], wordArr[wordArr.length - 1]] = [wordArr[wordArr.length - 1], wordArr[0]];
+//     return wordArr.join("");
+// }
+
+// console.log(replaceFirstWithLast("olzerE")); // Elzero
+// console.log(replaceFirstWithLast("Hello")); // oelloH
 
 //#endregion
-"=========================  6 ============================"
+"=========================  6 ============================";
 //#region ======>  6 <===========
+// function checkBiggestNum(word) {
+//     let result = word.split("").reduce((acc, ele) => acc > ele ? +acc : +ele);
+//     return result;
+// }
 
+// console.log(checkBiggestNum("1500654")); // 6
+// console.log(checkBiggestNum("8509507")); // 9
 //#endregion
-"=========================  7  ============================"
+"=========================  7  ============================";
 //#region ======>  7 <===========
-
+// solution 1
+// let nums = [20, 100, 50, 10, 15, -20, 30];
+//     let [, x, y, ,, ,] = nums
+//     console.log([x , y])
+"=================";
+// Solution Two 
+// let nums = [20, 100, 50, 10, 15, -20, 30];
+// let result = nums.sort( (a , b)=> b - a ).slice(0 , 2)
+// console.log(result)
+// Needed Output
+// [100, 50]
 //#endregion
-"=========================  8  ============================"
+"=========================  8  ============================";
 //#region ======>  8 <===========
 
+
+// Closest Number Is 88
 //#endregion
-"=========================  9  ============================"
+"=========================  9  ============================";
 //#region ======>  9 <===========
+// function swapEveryTwoChars(word) {
+//     let result = word.split("")
+//         .map((char, i) => i % 2 === 0 ?
+//             (char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase()) : char);
+
+//     return result.join("");
+
+// }
+
+// console.log(swapEveryTwoChars("elZeRo")); // Elzero
+// console.log(swapEveryTwoChars("heLlO")); // Hello
+//#endregion
+
+"=========================  10  ============================";
+//#region ======>  10 <===========
+
+// String.prototype.elzeroRepeat = function (count) {
+//     return this.repeat(count);
+// };
+
+// console.log("Elzero ".elzeroRepeat(3)); // Elzero Elzero Elzero
 
 //#endregion
-"=========================  10  ============================"
-//#region ======>  10 <===========
+
+"=========================  11  ============================";
+//#region ======>  11 <===========
+
+//#endregion
+"=========================  12  ============================";
+//#region ======>  12 <===========
+
+//#endregion
+"=========================  13  ============================";
+//#region ======>  13 <===========
+
+//#endregion
+"=========================  14  ============================";
+//#region ======>  14 <===========
+
+//#endregion
+"=========================  15  ============================";
+//#region ======>  15 <===========
+
+//#endregion
+"=========================  16  ============================";
+//#region ======>  16 <===========
+
+//#endregion
+"=========================  17  ============================";
+//#region ======>  17 <===========
+
+//#endregion
+"=========================  18  ============================";
+//#region ======>  18 <===========
+
+//#endregion
+"=========================  19  ============================";
+//#region ======>  19 <===========
+
+//#endregion
+"=========================  20  ============================";
+//#region ======>  20 <===========
 
 //#endregion
